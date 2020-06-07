b396295253bc0be6eabf439bfd3cd1920d5947f0
FlawwsX <55849405+FlawwsX@users.noreply.github.com>
2020-06-07 20:08:54 +0100

    More locations for deliveries and collecting cargo.

--- config.lua
+++ config.lua
@@ -13,10 +13,10 @@ CONST_DELIVERING     = 3
 
 Config = {}
 
-Config.AbortKey         = 167 -- (F6)
-Config.TruckRentalPrice = 500
-Config.TruckModel       = 'phantom'
-Config.PayPerMeter      = 0.10
+Config.AbortKey         = 73 -- (X)
+Config.TruckRentalPrice = 250
+Config.TruckModel       = 'phantom3'
+Config.PayPerMeter      = 0.7
 
 Config.JobStart = {
 	Coordinates = vector3(1189.61, -3104.31, 4.7),
@@ -43,8 +43,8 @@ Config.Marker = {
 Config.Routes = {
 	{
 		TrailerModel      = 'tr4',
-		PickupCoordinates = vector3(508.94, -3047.27, 6.32),
-		PickupHeading     = 0.0,
+		PickupCoordinates = vector3(509.23, -3039.27, 7.32),
+		PickupHeading     = 357.82,
 		Destinations = {
 			vector3(-15.93, -1104.25, 25.67), -- PDM
 			vector3(-810.84, -228.29, 36.21), -- Luxury Autos
@@ -57,17 +57,52 @@ Config.Routes = {
 		PickupHeading     = 79.30,
 		Destinations = {
 			vector3(985.74, -2523.91, 27.3), -- Cypress Warehouses
+			vector3(63.74, -325.91, 44.3), -- Middle of city, where wood is.
 		}
 	},
 	{
-		TrailerModel      = 'tanker',
-		PickupCoordinates = vector3(518.88, -2156.75, 4.99),
-		PickupHeading     = 172.43,
+		TrailerModel      = 'docktrailer',
+		PickupCoordinates = vector3(-646.10,-1726.81, 24.99),
+		PickupHeading     = 340.60,
 		Destinations = {
 			vector3(264.38, -1244.98, 28.14), -- Xero 24 (Postal 4027)
 			vector3(1780.95, 3330.17, 40.25), -- Gas (Postal 802)
 			vector3(-97.66, 6422.26, 30.43), -- Xero (Postal 911)
 			vector3(-1811.93, 810.2, 137.53), -- Xero (Postal 911)
+		}
+	},
+	{
+		TrailerModel      = 'trailers4',
+		PickupCoordinates = vector3(77.50,6325.53, 31.23),
+		PickupHeading     = 30.00,
+		Destinations = {
+			vector3(1907.21, 566.12, 175.82), -- Near Mining Place
+			vector3(1964.91, 4644.60, 40.88), -- Somewhere in Blaine.
+			vector3(1682.20, 3271.59, 40.75), -- Near Sandy Airport
+			vector3(1843.90, 2559.69, 45.67), -- Prison
+			vector3(24.90, -371.69, 39.38), -- City place.
+		}
+	},
+	{
+		TrailerModel      = 'trailers2',
+		PickupCoordinates = vector3(161.00,-2980.86, 5.89),
+		PickupHeading     = 267.96,
+		Destinations = {
+			vector3(1137.96, -292.31, 68.85), -- Some place.
+			vector3(1106.88, 212.87, 80.99), -- Behind CASINO
+			vector3(-388.78, 279.17, 84.70), --Some pleac.e
+			vector3(-306.85, -939.09, 31.08), -- Some other place.
+			vector3(-133.33, -262.51, 42.81), -- In front of cluckin' bell.
+		}
+	},
+	{
+		TrailerModel      = 'armytrailer',
+		PickupCoordinates = vector3(-338.16,6070.12, 31.30),
+		PickupHeading     = 222.23,
+		Destinations = {
+			vector3(821.50, -2142.59, 28.87), -- Ammuniation.
+			vector3(2554.38, 288.66, 108.46), -- Another gun stor.e
+			vector3(-1133.55, 2694.31, 18.80), -- Another gun store
 		}
 	},
 }
